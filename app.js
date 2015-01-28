25a10635f651d3eddcfa714585b668af345f87ca
philpill <github@philpill.net>
2015-01-28 22:52:52 +0000

    add cheerio and q for scraping

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var q = require('q');
+var scraper = require('./scraper');
 
 app.set('views', './views');
 app.set('view engine', 'jade');
@@ -7,7 +9,15 @@ app.set('view engine', 'jade');
 app.use('/static', express.static(__dirname + '/static'));
 
 app.get('/', function(req, res) {
-    res.render('index', { title: 'Hey', message: 'Hello Grid!'});
+
+    scraper.scrape()
+    .then(function (title) {
+
+        res.render('index', { title: 'Hey', message: title });
+    });
+
+
+
 });
 
 app.listen(3000);
---
+++ scraper.js
@@ -0,0 +1,101 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var q = require('q');
+
+function init () {
+
+    console.log('scraper.init()');
+}
+
+function getFeed () {
+
+    var deferred = q.defer();
+
+    request('http://feeds.bbci.co.uk/news/world/rss.xml', function (error, response, html) {
+
+        if (!error && response.statusCode === 200) {
+
+            var promises = [];
+
+            var $ = cheerio.load(html, {
+                xmlMode: true
+            });
+
+            var $links = $('channel > item > link');
+
+            var l = $links.length;
+
+            for (var i = 0; i < l; i++) {
+
+                promises[i] = getArticle($links.eq(i).text());
+            }
+
+            q.all(promises).spread(function () {
+
+                console.log([].slice.call(arguments));
+
+                deferred.resolve();
+            });
+        }
+    });
+
+    return deferred.promise;
+}
+
+function getArticle (url) {
+
+    var deferred = q.defer();
+
+    request(url, function (error, response, html) {
+        if (!error && response.statusCode === 200) {
+
+            // console.log(url);
+
+            var $ = cheerio.load(html);
+
+            var $headline = $('.story-header, .story-body__h1, .article p.introduction');
+
+            if ($headline.length > 0) {
+
+                $headline.filter(function(){
+
+                    var data = $(this);
+
+                    var title = data.text().trim();
+
+                    // console.log(title);
+
+                    deferred.resolve(title);
+                });
+
+            } else {
+
+                deferred.reject();
+            }
+
+        }
+    });
+
+    return deferred.promise;
+}
+
+function scrape () {
+
+    console.log('scraper.scrape()');
+
+    var deferred = q.defer();
+
+    getFeed().then(function () {
+
+        deferred.resolve();
+    });
+
+    return deferred.promise;
+}
+
+
+
+module.exports = {
+
+    scrape : scrape
+};
