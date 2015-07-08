a910c9e20eeac4226c9a63206958b3ff0d7980e3
philpill <github@philpill.net>
2015-07-09 00:09:09 +0100

    add locations to analysis

--- scraper.analyse.aylien.js
+++ scraper.analyse.aylien.js
@@ -19,15 +19,13 @@ var api = new aylienApi({ application_id: id, application_key: key });
 
 */
 
-function aylien (article) {
-
-    console.log('scraper.analyse.aylien()');
+function getClassification (story) {
 
     var deferred = q.defer();
 
     api.classify({
 
-        text : article.data.story
+        text : story
 
     }, function(error, response) {
 
@@ -39,13 +37,58 @@ function aylien (article) {
 
             console.log(response.categories);
 
-            article.categories = response.categories;
-
-            deferred.resolve(article);
+            deferred.resolve(response.categories);
         }
     });
 
     return deferred.promise;
+}
+
+function getLocations (story) {
+
+    api.entities({
+
+        text : story
+
+    }, function(error, response) {
+
+        if (error) {
+
+            deferred.reject(error);
+
+        } else {
+
+            console.log(response.entities.location);
+
+            deferred.resolve(response.entities.location);
+        }
+    });
+}
+
+function aylien (article) {
+
+    console.log('scraper.analyse.aylien()');
+
+    var deferred = q.defer();
+
+    var story = article.data.story;
+
+    q.spread([getClassification(story), getLocations(story)], function (classification, locations) {
+
+        article.analysis.locations = locations;
+
+        article.analysis.classification = classification;
+
+        defer.resolve(article);
+
+    }, function (error) {
+
+        console.log(error);
+
+        defer.reject();
+    });
+
+    return defer.promise;
 }
 
 module.exports = aylien;
