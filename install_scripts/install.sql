b49aba1fce52688100af0a52b4c8d101041ad6b4
philpill <github@philpill.net>
2015-07-11 15:41:57 +0100

    Update database script

--- install_scripts/install.sql
+++ install_scripts/install.sql
@@ -1,6 +1,6 @@
---DROP TABLE IF EXISTS country, article, article_category, article_country, analysis;
+--DROP TABLE IF EXISTS location, article, article_category, article_country;
 
-CREATE TABLE IF NOT EXISTS country (id SERIAL PRIMARY KEY, name text NOT NULL);
+CREATE TABLE IF NOT EXISTS location (id SERIAL PRIMARY KEY, name text NOT NULL);
 
 CREATE TABLE IF NOT EXISTS article (id SERIAL PRIMARY KEY, domain text NOT NULL, url text NOT NULL, date date NOT NULL, content text, article text);
 
@@ -8,6 +8,4 @@ CREATE TABLE IF NOT EXISTS category (id SERIAL PRIMARY KEY, code text NOT NULL, 
 
 CREATE TABLE IF NOT EXISTS article_category (article_id SERIAL REFERENCES article(id), category_id SERIAL REFERENCES category(id), PRIMARY KEY (article_id, category_id) );
 
-CREATE TABLE IF NOT EXISTS article_country (article_id SERIAL REFERENCES article(id), country_id SERIAL REFERENCES country(id), PRIMARY KEY (article_id, country_id) );
-
-CREATE TABLE IF NOT EXISTS analysis (id SERIAL PRIMARY KEY, article_id SERIAL REFERENCES article(id), data text);
+CREATE TABLE IF NOT EXISTS article_location (article_id SERIAL REFERENCES article(id), location_id SERIAL REFERENCES location(id), PRIMARY KEY (article_id, location_id) );
