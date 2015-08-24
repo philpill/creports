d720ca004ed25885949b492374a83b801174dbe6
philpill <github@philpill.net>
2015-08-24 22:33:11 +0100

    refactor to use marionette

--- src/js/app.js
+++ src/js/app.js
@@ -1,175 +1,213 @@
 (function (document, articles) {
 
-    var $ = require('jquery.min');
-    var _ = require('lodash.min');
+    var $ = require('jquery.min'),
+        _ = require('lodash.min'),
+        React = require('react.min'),
+        Backbone = require('backbone-min'),
+        Marionette = require('backbone.marionette.min');
 
     require('d3.min');
     require('topojson');
     require('datamaps.all.min');
 
+    var app = new Marionette.Application();
 
-    require('backbone-min');
-    require('backbone.marionette.min');
-
-    require('react.min');
-
-    var map;
-
-    var articlesByCountry = {};
-
-    var countries = {};
-
-    var geometries = Datamap.prototype.worldTopo.objects.world.geometries;
-
-    var countryCodes = _.filter(_.map(geometries, getCountryCodeFromGeometryData), filterDisputedCountries);
-
-    _.each(countryCodes, function (code) {
-        countries[code] = 0;
+    app.addRegions({
+        appRegion: '#App'
     });
 
-    console.log(countries);
+    // following example:
+    // http://www.eccesignum.org/blog/simple-marionettejs-model-collection-and-collectionview-example
+    app.module('App',function(module, App, Backbone, Marionette, $, _) {
 
-    // countries which are disputed (e.g. Kosovo) have country codes of -99 which we cannot parse
-    function filterDisputedCountries (id) {
-        return id.match(/^[A-Z][A-Z][A-Z]$/);
-    }
+        module.AppLayoutView = Marionette.LayoutView.extend({
 
-    function getCountryCodeFromGeometryData (geometry) {
-        return geometry.id;
-    }
+            template: '#MapTemplate',
 
-    function init () {
+            initialize: function() { console.log('main layout: initialize'); },
+            onRender: function() { console.log('main layout: onRender'); },
 
-        console.log('conflicting reports');
+            onShow: function() {
 
-        console.log(articles);
+                console.log('main layout: onShow');
 
-        var $container = $('#Map');
+                var map, geometries,
+                    countryCodes, articlesByCountry,
+                    countries;
 
-        map = new Datamap({
-            element : $container[0],
-            responsive : true,
-            projection: 'mercator',
-            geographyConfig : {
-                borderColor: 'Green',
-                highlightFillColor : 'Green',
-                highlightBorderWidth : 1,
-                popupOnHover: false
-            },
-            fills : {
-                defaultFill : 'black'
-            },
-            done: function(datamap) {
+                function initialiseData () {
 
-                datamap.svg.selectAll('.datamaps-subunit').on('click', onDatamapCountryClick);
-            }
-        });
+                    countries = countries || {};
 
-        updateMap(articles);
+                    geometries = Datamap.prototype.worldTopo.objects.world.geometries;
 
-        bindEvents();
-    }
+                    countryCodes = _.filter(_.map(geometries, getCountryCodeFromGeometryData), filterDisputedCountries);
 
-    function onDatamapCountryClick (geography) {
-
-        console.log(geography);
-
-        $('#Stories').removeClass('active');
-
-        setTimeout(function () {
-
-            var articles = articlesByCountry[geography.id];
-
-            if (articles && articles.length > 0) {
-
-                $('#Stories').addClass('active');
-
-                document.getElementById('Articles').innerHTML = '';
-
-                articles.forEach(function (article) {
-
-                    document.getElementById('Country').innerHTML = geography.properties.name;
-
-                    document.getElementById('Articles').innerHTML += '<li><a href="' + article.url + '">' + article.headline + '</a></li>';
-                });
-            }
-
-        }, 600);
-    }
-
-    function bindEvents () {
-
-        $(window).on('resize', map.resize.bind(map));
-
-        $('.source-check').on('change', updateSources);
-    }
-
-    function updateSources () {
-
-        var filteredArticles = [];
-
-        var sources = $('.source-check:checked');
-
-        _.each(articles, function (article) {
-
-            _.each(sources, function (source) {
-
-                if (source.value === article.source) {
-
-                    filteredArticles.push(article);
-                }
-            });
-        });
-
-        clearData();
-        updateMap(filteredArticles);
-    }
-
-    function clearData () {
-
-        _.each(countryCodes, function (code) {
-
-            countries[code] = 0;
-        });
-
-        articlesByCountry = {};
-    }
-
-    function updateMap (articles) {
-
-        console.log('updateMap()');
-
-        articles.forEach(function (article) {
-
-            article.countries.forEach(function (country) {
-                if (country.code) {
-                    articlesByCountry[country.code] = articlesByCountry[country.code] || [];
-                    articlesByCountry[country.code].push({
-                        url : article.url,
-                        headline : article.headline,
-                        story : article.story
+                    _.each(countryCodes, function (code) {
+                        countries[code] = 0;
                     });
-                    countries[country.code] = countries[country.code] ? countries[country.code] + 0.15 : 0.15;
+
+                    console.log(countries);
                 }
-            });
+
+                function createMap (container) {
+
+                    var options = {
+                        element : container,
+                        responsive : true,
+                        projection: 'mercator',
+                        geographyConfig : {
+                            borderColor: 'Green',
+                            highlightFillColor : 'Green',
+                            highlightBorderWidth : 1,
+                            popupOnHover: false
+                        },
+                        fills : {
+                            defaultFill : 'black'
+                        },
+                        done: function(datamap) {
+
+                            datamap.svg.selectAll('.datamaps-subunit').on('click', onDatamapCountryClick);
+                        }
+                    };
+
+                    return new Datamap(options);
+                }
+
+                // countries which are disputed (e.g. Kosovo) have country codes of -99 which we cannot parse
+                function filterDisputedCountries (id) {
+                    return id.match(/^[A-Z][A-Z][A-Z]$/);
+                }
+
+                function getCountryCodeFromGeometryData (geometry) {
+                    return geometry.id;
+                }
+
+                function onDatamapCountryClick (geography) {
+
+                    console.log(geography);
+
+                    $('#Stories').removeClass('active');
+
+                    setTimeout(function () {
+
+                        var articles = articlesByCountry[geography.id];
+
+                        if (articles && articles.length > 0) {
+
+                            $('#Stories').addClass('active');
+
+                            document.getElementById('Articles').innerHTML = '';
+
+                            articles.forEach(function (article) {
+
+                                document.getElementById('Country').innerHTML = geography.properties.name;
+
+                                document.getElementById('Articles').innerHTML += '<li><a href="' + article.url + '">' + article.headline + '</a></li>';
+                            });
+                        }
+
+                    }, 600);
+                }
+
+                function bindEvents () {
+
+                    $(window).on('resize', map.resize.bind(map));
+
+                    $('.source-check').on('change', updateSources);
+                }
+
+                function updateSources () {
+
+                    var filteredArticles = [];
+
+                    var sources = $('.source-check:checked');
+
+                    _.each(articles, function (article) {
+
+                        _.each(sources, function (source) {
+
+                            if (source.value === article.source) {
+
+                                filteredArticles.push(article);
+                            }
+                        });
+                    });
+
+                    clearData();
+
+                    updateMap(filteredArticles);
+                }
+
+                function clearData () {
+
+                    _.each(countryCodes, function (code) {
+
+                        countries[code] = 0;
+                    });
+
+                    articlesByCountry = {};
+                }
+
+                function updateMap (articles) {
+
+                    console.log('updateMap()');
+
+                    articlesByCountry = articlesByCountry || {};
+
+                    articles.forEach(function (article) {
+
+                        article.countries.forEach(function (country) {
+                            if (country.code) {
+                                articlesByCountry[country.code] = articlesByCountry[country.code] || [];
+                                articlesByCountry[country.code].push({
+                                    url : article.url,
+                                    headline : article.headline,
+                                    story : article.story
+                                });
+                                countries[country.code] = countries[country.code] ? countries[country.code] + 0.15 : 0.15;
+                            }
+                        });
+                    });
+
+                    Object.keys(countries).forEach(function(country, index) {
+
+                        var frequency = countries[country];
+
+                        var color = 'rgba(255, 0, 0, ' + frequency + ')';
+
+                        countries[country] = color;
+                    });
+
+                    console.log(countries);
+
+                    console.log(articlesByCountry);
+
+                    map.updateChoropleth(countries);
+                }
+
+                initialiseData();
+
+                map = createMap($('#Map')[0]);
+
+                updateMap(articles);
+
+                bindEvents();
+            }
         });
 
-        Object.keys(countries).forEach(function(country, index) {
+        module.addInitializer(function() {
 
-            var frequency = countries[country];
+            console.log('conflicting reports');
 
-            var color = 'rgba(255, 0, 0, ' + frequency + ')';
+            console.log(articles);
 
-            countries[country] = color;
+            var layout = new module.AppLayoutView();
+
+            app.appRegion.show(layout);
         });
+    });
 
-        console.log(countries);
-
-        console.log(articlesByCountry);
-
-        map.updateChoropleth(countries);
-    }
-
-  init();
+    $(document).ready(function() { app.start(); });
 
 })(document, articles);
